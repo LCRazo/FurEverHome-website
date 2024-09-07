f54bba1cbb7eace8158cbff498f3c834d6ae5748
Lizeth Chavez <115516273+LCRazo@users.noreply.github.com>
2024-09-07 15:59:19 -0500

    Added footer content- missing logo,license,estimate form link and social media links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,25 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Footer(){
     return(
-        <div className="footer-container"> 
-            <div className='text-center footer-content'>
+        <div className="footer-container pt-5"> 
+            <div className='text-left footer-content'>
                 <div className='d-flex justify-content-center pr-5'>
-                <div className="footer-section info pr-5">
+                       
+                <div className="footer-section about-us pr-5 ">
+                    <h4>About Us</h4>
+                        <p>
+                            We're your friendly local and family-owned roofing company 
+                            with 15+ years of experience delivering superior exterior home 
+                            improvement solutions for the RGV.
+                        </p>
+                       
+                </div>
+
+                <div className="footer-section info pr-5 ">
                     <h4>Contact Info</h4>
-                        <p>Email: roofing@gmail.com</p>
-                        <p>Phone: 9566623921</p>
+                        <p>roofing@gmail.com</p>
+                        <p>9566623921</p>
+                        <p>Office Hours:</p>
                 </div>
 
                 <div className='footer-services pr-5'>
@@ -29,10 +41,13 @@ function Footer(){
                         <a href="#"><i className="fa-brands fa-instagram"></i></a>
                         <a href="#"><i className="fa-brands fa-facebook"></i></a>
                 </div>
-
+               
                 {/* Add API form here*/}
                 </div>
             </div>
+            <div className='footer-section copyright d-flex align-items-center flex-column bd-highlight mt-3'>
+                    <p>© 2024 <strong>CS&More Roofing Company</strong>. All Rights Reserved</p>
+                </div>
         </div>
     )
 }
