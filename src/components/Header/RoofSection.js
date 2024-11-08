b40ade8b12a535a2b93285976f561295802d8042
Lizeth Chavez <115516273+LCRazo@users.noreply.github.com>
2024-11-08 00:03:56 -0600

    Footer Alignment Fixed

--- src/components/Header/RoofSection.js
+++ src/components/Header/RoofSection.js
@@ -3,14 +3,16 @@ import './RoofSection.css'
 
 function RoofSection(){
     return(
-        <div className="roof-section d-flex justify-content-center align-items-center">
+        <div className="roof-container container-fluid">
            <div className='text-center text-white'>
+           <div className="roof-section col-12 pt-5">
                 <h1 className="roof-title"> ROOF SMART</h1>
                 <h3 className="roof-subtitle">Residential & Commercial</h3>
                 <h4 className="roof-description">Family-owned roofing experts in the RGV! We deliver reliable, high-quality service with a personal touch, treating your roof like it’s our own.</h4>
                 <a type="button" class="btn btn-secondary" href="assets/LizethChavez.pdf" target="_blank" download>Request Free Estimate</a>
            </div>
         </div>
+    </div>
     );
 }
 
