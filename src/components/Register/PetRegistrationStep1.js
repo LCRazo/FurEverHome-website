e7717b1e86131efe91dbe038b11a9f58f27de875
Lizeth Chavez <115516273+LCRazo@users.noreply.github.com>
2025-05-05 19:15:00 -0500

    input design

--- src/components/Register/PetRegistrationStep1.js
+++ src/components/Register/PetRegistrationStep1.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Logo from '../assets/petRegisterLogo.svg'
 import {useNavigate} from 'react-router-dom';
 
 function PetRegistrationStep1() {
@@ -6,14 +7,95 @@ function PetRegistrationStep1() {
   const navigate = useNavigate();
 
   const handleNext = () => {
-      navigate('/api/pets/register/step2');
+      const { webPhoto, ...textData } = formData;
+
+      navigate('/api/pets/register/step2', {state:{...textData, webPhoto}});
+
   };
 
+  const handleChange = (e) => {
+      setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  const [formData, setFormData] = useState({
+    name: '',
+    species: '',
+    breed: '',
+    workingClass: '',
+    urgency: '',
+    webPhoto: ''
+  });
+
+
+
   return(
-    <section id="pets" className= "bg-pink-300 py-12 px-4 text-center ">
-        <button onClick={handleNext} >
-          Next
-        </button>
+    <section className= "bg-pink-300 min-h-screen py-12 px-4 text-center text-white font-saira text-xl ">
+      <div classname='' >
+          {/*Title */}
+        <div className='flex flex-col items-center mb-4'>
+            <img src={Logo}></img>
+            <p className='pt-4'>Let’s get to know your pet! Fill out the following background information.</p>
+        </div>
+
+        {/*Form */}
+        <div className='flex-col items-center'>
+          <label className="block text-left text-white">Name*</label>
+          <input 
+                name="name" 
+                value={formData.name} 
+                onChange={handleChange} 
+                className="block w-full mb-2 p-2 text-black" 
+                required/>
+          
+          <label className="block text-left text-white">Species*</label>
+          <input 
+                name="species" 
+                value={formData.species} 
+                onChange={handleChange} 
+                className="block w-full mb-2 p-2 text-black" 
+                required/>
+          
+          <label className="block text-left text-white">Breed*</label>
+          <input 
+                name="breed"  
+                value={formData.breed} 
+                onChange={handleChange} 
+                className="block w-full mb-2 p-2 text-black"
+                required/>
+          
+          <label className="block text-left text-white">Is your pet a working class breed?*</label>
+          <input 
+                name="workingClass"  
+                value={formData.workingClass} 
+                onChange={handleChange} 
+                className="block w-full mb-2 p-2 text-black" 
+                required/>
+
+          <label className="block text-left text-white">How long can you care for your pet?*</label>
+          <input  
+                name="urgency"  
+                value={formData.urgency} 
+                onChange={handleChange}
+                className="block w-full mb-2 p-2 text-black rounded" 
+                required/>
+
+          <label className="block text-left text-white">Web Photo*</label>
+              <input
+                name="webPhoto"
+                type="file"
+                accept="image/*"
+                onChange={(e) => setFormData({...formData, webPhoto: e.target.files[0]})}
+                className="block w-full mb-2 p-2 text-black bg-white rounded"
+              />
+        </div>
+
+        {/*Buttons */}
+        <div className='pt-4 flex justify-center space-x-4'>
+          <button onClick={handleNext} className='bg-white text-pink-600 px-4 py-2 rounded'>Next</button>
+        </div>
+        
+      </div>
+      
     </section>
   );
 };
--- src/components/Register/PetRegistrationStep2.js
+++ src/components/Register/PetRegistrationStep2.js
@@ -1,13 +1,114 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Logo from '../assets/petRegisterLogo.svg'
+import { useNavigate } from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
 
 function PetRegistrationStep2() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const step1Data = location.state || {};
 
+  const [formData, setFormData] = useState({
+      gender: '',
+      size: '',
+      age: '',
+      vaccinated: '',
+      fixed: ''
+    });
+
+    const handleChange = (e) => {
+      setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async () => {
+      const step1Data = JSON.parse(localStorage.getItem('petStep1'));
+      const combinedData = {...step1Data,...formData};
+
+      const formToSend = new FormData();
+
+      //attach text fields
+      for(const key in combinedData){
+        formToSend.append(key, combinedData[key]);
+      }
+
+      //attach the image seperate
+      if (step1Data.webPhoto && typeof step1Data.webPhoto === 'object'){
+        formToSend.append('webPhoto', step1Data.webPhoto);
+      }
+      
+      try {
+        const res = await fetch('/register/pet/step2', {
+          method: 'POST',
+          body: formToSend
+        });
+  
+        if (res.ok) {
+          localStorage.removeItem('petStep1');
+          navigate('/'); // or another success page
+        } else {
+          console.error('Upload failed');
+        }
+      } catch (err) {
+        console.error('Error:', err);
+      }
+  
+    };
+  
 
   return(
-    <section id="pets" className= "bg-pink-200 py-12 px-4 text-center ">
-        <span>
-            part two of pet registration 
-        </span>
+    <section className= "bg-pink-300 min-h-screen py-12 px-4 text-center text-white font-saira text-xl ">
+          {/*Title */}
+        <div className='flex flex-col items-center mb-4'>
+            <img src={Logo}></img>
+            <p className='pt-4'>Now fill out the information regarding your pet’s health</p>
+        </div>
+         {/*Form */}
+         <div className='flex-col items-center'>
+          <label className="block text-left text-white">Gender*</label>
+          <input 
+              name="gender"
+              value= {formData.gender}
+              onChange={handleChange} 
+              className="block w-full mb-2 p-2 text-black"
+              required/>
+
+          <label className="block text-left text-white">Size*</label>
+          <input 
+              name="size" 
+              value={formData.size}
+              onChange={handleChange}
+              className="block w-full mb-2 p-2 text-black"
+              required/>
+
+          <label className="block text-left text-white">Age*</label>
+          <input 
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
+              className="block w-full mb-2 p-2 text-black"
+              required/>
+
+          <label className="block text-left text-white">Is your pet vaccinated?*</label>
+          <input 
+              name="vaccinated" 
+              value={formData.vaccinated}
+              onChange={handleChange}
+              className="block w-full mb-2 p-2 text-black"
+              required/>
+
+          <label className="block text-left text-white">Is your pet spayed or neutered?*</label>
+          <input 
+              name="fixed" 
+              value={formData.fixed}
+              onChange={handleChange}
+              className="block w-full mb-2 p-2 text-black"
+              required/>
+
+        </div>
+         {/*Buttons */}
+         <div className='pt-4'>
+          <button className='bg-white text-pink-600 px-4 py-2 rounded' onClick={handleSubmit}>Submit</button>
+        </div>
     </section>
   );
 };
