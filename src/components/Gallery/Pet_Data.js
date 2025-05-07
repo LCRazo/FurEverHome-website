18d2dbf9b1e67d1d857d11b56e2ac139fab11121
belindaalvarado <alvaradobelinda653@gmail.com>
2025-05-06 19:40:05 -0500

    feat: added mobile views for pet gallery, pending filter functionality/connection to db

--- src/components/Gallery/Pet_Data.js
+++ src/components/Gallery/Pet_Data.js
@@ -1,5 +1,8 @@
 import tobyImg from '../assets/Stock_Pet_Profile_Images/toby-dalmatian.jpg';
 import mochiImg from '../assets/Stock_Pet_Profile_Images/mochi-cat.svg';
+import billyImg from '../assets/Stock_Pet_Profile_Images/billy-goat.svg';
+import charImg from '../assets/Stock_Pet_Profile_Images/charlotte-pig.svg';
+import remyImg from '../assets/Stock_Pet_Profile_Images/remy-rat.png';
 
 const pets = [
   {
@@ -16,7 +19,7 @@ const pets = [
       "Toby is a bright, affectionate Dalmatian pup with a playful spirit and a heart full of love. Whether he's racing along the creek or curling up in your lap for a nap, he's happiest wherever his people are.",
   },
   {
-    id: 1,
+    id: 2,
     name: 'Mochi',
     type: 'cat',
     breed: 'American Shorthair',
@@ -27,6 +30,43 @@ const pets = [
     img: mochiImg,
     description: " Meet Mochi, she's independent but cuddly, loves a good lounge session in a sunny window. "
   },
+  {
+    id: 3,
+    name: 'Billy',
+    type: 'goat',
+    breed: 'American Shorthair',
+    age: '1 years old',
+    gender: 'female',
+    vaccination:'not vaccinated',
+    sterilized: 'spayed',
+    img: billyImg,
+    description: " Meet Billy, she's independent but cuddly, loves a good lounge session in a sunny window. "
+  },
+  {
+    id: 4,
+    name: 'Charlotte',
+    type: 'pig',
+    breed: 'American Shorthair',
+    age: '2 years old',
+    gender: 'female',
+    vaccination:'vaccinated',
+    sterilized: 'not spayed',
+    img: charImg,
+    description: " Meet Charlotte, she's independent but cuddly, loves a good lounge session in a sunny window. "
+  },
+  {
+    id:5,
+    name: 'Remy',
+    type: 'rat',
+    breed: 'Brown rat',
+    age: ' 3 years old',
+    gender: 'male',
+    vaccination: 'vaccinated',
+    sterilized: 'not neutered',
+    img: remyImg,
+    description: "Meet Remy, Like from ratatouille!"
+  }
+  
   // Add more pets...
 ];
 
--- src/components/Gallery/Pet_Profile_Modal.js
+++ src/components/Gallery/Pet_Profile_Modal.js
@@ -3,18 +3,18 @@ import React from 'react';
 function PetProfileModal({ pet, onClose }) {
     return (
       <div className="fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center p-4">
-        <div className="relative bg-purple-500 rounded-2xl p-6 w-full max-w-2xl flex flex-col-reverse md:flex-row-reverse gap-6 text-left text-white overflow-y-auto max-h-[90vh]">
+        <div className="relative bg-purple-500 rounded-2xl p-6 w-full max-w-2xl flex flex-col md:flex-row-reverse gap-6 text-left text-white overflow-y-auto max-h-[90vh]">
           {/* Close Button */}
           <button 
           onClick={onClose} 
           className="absolute top-0 right-2 text-3xl hover:text-red-400">&times;</button>
   
           {/* Pet Image */}
-          <div className="w-full md:w-1/2 aspect-square overflow-hidden rounded-xl">
+          <div className="w-full md:w-1/2 h-1/2 aspect-square rounded-xl self-center">
             <img
               src={pet.img}
               alt={pet.name}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover rounded-xl"
             />
           </div>
           {/* Pet Info */}
