fe52e4c5cb1aaac513729df7d812dc979ade9ff5
Adrian Pena <penajakk@gmail.com>
2025-05-09 21:52:05 -0500

    queries queries queries

--- backend/db.js
+++ backend/db.js
@@ -1,43 +1,17 @@
-//const { Sequelize } = require('sequelize');
+// db.js
+require('dotenv').config();
+const mysql = require("mysql2/promise");
 
-//const sequelize = new Sequelize('your_db_name', 'your_username', 'your_password', {
-//  host: 'your-aws-endpoint.amazonaws.com',
-//  dialect: 'mysql',
-//});
+async function createConnection() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
 
-//module.exports = sequelize;
-DB_HOST='db-class-instance.csjyam0mi670.us-east-1.rds.amazonaws.com'
-DB_USER='admin'
-DB_PASSWORD='password'
-DB_NAME='db-class-instance'
+  console.log("Connected to database.");
+  return connection;
+}
 
-const mysql = require("mysql");
-
-//async function initializeConnection() {
-//  try{
-var connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-    
-  console.log('Connected to database.');
-});
-//    const connection = await mysql.createConnection(connectionConfig);
-//    console.log('Connected to ${connectionConfig.database} database');
-
-//    return connection;
-//  } catch(err) {
-//    console.error("Error connecting to database: ", err);
-//    throw err;
-//  }
-//}
-connection.end
-
-module.exports = {initializeConnection}
+module.exports = createConnection; // exporting the function, not the result
